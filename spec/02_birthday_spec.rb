147c18e9e2e1d8f4b54878baa4d2fac5b717d1af
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-21 22:59:43 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -22,6 +22,16 @@ describe "#age_appropriate_birthday" do
       "Sarah" => 6,
       "Amanda" => 27
     }
+  def age_appropriate_birthday(birthday_kids)
+
+  birthday_kids.each do |kids_name, age|
+    if age <=12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
 
     expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
